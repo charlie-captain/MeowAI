dfedb9d73d34edbf566cf56b7bd0bfd48cc79877
charlie-captain <chavezayne@gmail.com>
2023-03-20 12:27:17 +0800

    bump : 2023-03-20 12:27:17

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git reset --h origin/main
 git pull
-chmod 777 ./run_docker.sh
+chmod 777 ./deploy.sh
 docker build --no-cache -t meowai_image .
 docker stop meow_ai || true
 docker rm meow_ai || true
