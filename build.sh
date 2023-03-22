ec4d7c00f471ea04f264696106ae863f0b911592
charlie-captain <chavezayne@gmail.com>
2023-03-22 10:20:59 +0800

    bump : 2023-03-22 10:20:59

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 git reset --h origin/main
 git pull
-chmod 777 ./deploy.sh
+chmod 777 ./build.sh
 docker build --no-cache -t meowai_image .
-docker stop meow_ai || true
-docker rm meow_ai || true
+docker stop meow || true
+docker rm meow || true
 #docker system prune -f || true
-docker run -it --name meow_ai --network host -v /volume1/photo:/app/data -v /volume1/docker/github/MeowAI/results:/app/results  meowai_image
+docker run -it --name meow --network host -v /volume1/photo:/app/data -v /volume1/docker/github/MeowAI/results:/app/results  meowai_image
